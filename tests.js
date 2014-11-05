9f7ea6b262cd8836c01853ee417f15ba7d394602
Eliza Brock <eliza@elizabrocksoftware.com>
2014-11-05 12:09:29 -0600

    Initial commit of in-class live coding.

---
+++ tests.js
@@ -0,0 +1,14 @@
+QUnit.test( "preamble(name)", function( assert ) {
+  assert.equal(preamble("Eliza"), "Your name is Eliza!");
+  assert.equal(preamble("Colby"), "Your name is Colby!");
+});
+
+QUnit.test("cheerFor(name)", function(assert){
+  expectedCheer = "Give me a... A!\nGive me a... L!";
+  assert.equal(cheerFor("AL"), expectedCheer);
+
+  expectedCheer = "Give me a... B!\nGive me a... O!\nGive me a... B!";
+  assert.equal(cheerFor("BOB"), expectedCheer);
+
+	// TODO: Names with spaces or hyphens; Empty strings; Numbers; The business with vowel sounds; Names with lowercase letters
+});
